bab8af6021724801632ba7d9c151924a6730563e
allan355 <allanbm13@hotmail.com>
2019-10-10 16:06:53 -0600

    Fix

---
+++ Data/Logica/Manejadores/TipoUsuario/TipoUsuarioLogica.vb
@@ -0,0 +1,8 @@
+﻿Imports Data
+
+Public Class TipoUsuarioLogica
+    Public Shared Function ObtenerTipoUsuario() As List(Of TipoUsuario)
+        Dim context As dataEntities = New dataEntities()
+        Return TipoUsuarioManejador.ObtenerTipoUsuario(context)
+    End Function
+End Class
---
+++ Data/WindowsApp1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Imports Logica
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.DataSource = TipoUsuarioLogica.ObtenerTipoUsuario()
+        ComboBox1.ValueMember = "Id"
+        ComboBox1.DisplayMember = "Descripcion"
+    End Sub
+End Class
