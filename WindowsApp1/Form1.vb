f629fb6dc7f102427dc552b16b9e27339ad90612
allan355 <allanbm13@hotmail.com>
2019-10-11 08:05:12 -0700

    Sample combobox

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,3 +1,9 @@
-﻿Public Class Form1
+﻿Imports Logica
 
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.DataSource = TipoUsuarioLogica.ObtenerTipoUsuario()
+        ComboBox1.ValueMember = "Id"
+        ComboBox1.DisplayMember = "Descripcion"
+    End Sub
 End Class
