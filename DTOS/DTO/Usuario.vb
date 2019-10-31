c6610cfcb56be2ec9dce5e1516582f3f060e8a4a
Allan Fernando Bejarano Montero <allanbm13@hotmail.com>
2019-10-30 21:17:18 -0600

    fix

--- DTOS/DTO/Usuario.vb
+++ DTOS/DTO/Usuario.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Usuario
-
+    Public Property Id As Integer
+    Public Property Nombre As String
+    Public Property Apellido As String
+    Public Property GeneroId As Integer
+    Public Property TipoUsuarioId As Integer
 End Class
---
+++ Data/Conexion/Usuario.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Usuario
+
+    Shared Function ObtenerTodo() As List(Of DTOS.Usuario)
+        Dim Con As Conexion = New Conexion()
+        Dim cmd As SqlDataAdapter = New SqlDataAdapter()
+        Dim DS As New DataSet()
+        Dim List As New List(Of DTOS.Usuario)
+        If Con.Open() Then
+            cmd.SelectCommand = New SqlCommand("SELECT * FROM usuario", Con.ObtenerConexion())
+            cmd.Fill(DS)
+            For i As Integer = 0 To DS.Tables(0).Rows.Count() - 1 Step 1
+                Dim U As New DTOS.Usuario()
+                U.Id = DS.Tables(0).Rows(i)("Id")
+                U.Nombre = DS.Tables(0).Rows(i)("Nombre")
+                U.GeneroId = DS.Tables(0).Rows(i)("GeneroId")
+                U.TipoUsuarioId = DS.Tables(0).Rows(i)("TipoUsuarioId")
+                List.Add(U)
+            Next
+        End If
+        Return List
+    End Function
+    Shared Function ObtenerUsuario(ByVal _id As Integer) As DTOS.Usuario
+        Dim Con As Conexion = New Conexion()
+        Dim cmd As SqlDataAdapter = New SqlDataAdapter()
+        Dim DS As New DataSet()
+        Dim U As New DTOS.Usuario()
+        If Con.Open() Then
+            cmd.SelectCommand = New SqlCommand("SELECT * FROM usuario where id=" & _id, Con.ObtenerConexion())
+            cmd.Fill(DS)
+            For i As Integer = 0 To DS.Tables(0).Rows.Count() - 1 Step 1
+                U.Id = DS.Tables(0).Rows(i)("Id")
+                U.Nombre = DS.Tables(0).Rows(i)("Nombre")
+                U.GeneroId = DS.Tables(0).Rows(i)("GeneroId")
+                U.TipoUsuarioId = DS.Tables(0).Rows(i)("TipoUsuarioId")
+                Exit For
+            Next
+        End If
+        Return U
+    End Function
+
+
+    Shared Function GuardarUsuario(ByVal _U As DTOS.Usuario) As Boolean
+        Dim Con As Conexion = New Conexion()
+        Dim cmd As SqlCommand = New SqlCommand()
+        Dim List As New List(Of DTOS.Usuario)
+        If Con.Open() Then
+            cmd.Connection = Con.ObtenerConexion()
+            cmd.CommandText = "insert into USUARIO(Nombre,Apellido,GeneroId,TipoUsuarioId) values('" & _U.Nombre & "','" & _U.Apellido & "'," & _U.GeneroId & "," & _U.TipoUsuarioId & ")"
+            cmd.ExecuteNonQuery()
+            Return True
+        End If
+        Return False
+    End Function
+End Class
